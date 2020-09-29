7479feea86a6caf947af50258d0269a793167b9c
CobuAdam <71340963+CobuAdam@users.noreply.github.com>
2020-09-29 18:21:55 -0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,9 @@
+total_seconds=2**64
+total_min=total_seconds//60
+total_h=total_min//60
+total_d=total_h//24
+total_y=total_d//365
+total_gen=total_y//99
+
+answer=input("how old are you")
+print("your age:",answer)
